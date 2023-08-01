2e98c53be06b57603639e5a1c52c60fe2575da72
Aman Karmani <aman@tmm1.net>
2023-08-01 05:24:12 +0000

    try training llama2

--- scripts/train_lora.sh
+++ scripts/train_lora.sh
@@ -1,29 +1,28 @@
 deepspeed fastchat/train/train_lora.py \
-    --model_name_or_path lmsys/vicuna-7b-v1.3  \
-    --lora_r 8 \
+    --model_name_or_path meta-llama/Llama-2-7b-chat-hf  \
+    --lora_r 64 \
     --lora_alpha 16 \
     --lora_dropout 0.05 \
-    --data_path $DATA_PATH \
+    --data_path data/dummy_conversation.json \
     --output_dir ./checkpoints \
-    --num_train_epochs 150 \
-    --fp16 True \
+    --num_train_epochs 3 \
+    --bf16 True \
+    --tf32 True \
     --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 2 \
+    --per_device_eval_batch_size 0 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "steps" \
     --eval_steps 100  \
     --save_strategy "steps" \
     --save_steps 200 \
     --save_total_limit 2 \
-    --learning_rate 2e-5 \
+    --learning_rate 1e-5 \
     --weight_decay 0. \
-    --warmup_ratio 0.03 \
+    --warmup_ratio 0. \
     --lr_scheduler_type "cosine" \
     --logging_strategy "steps" \
     --logging_steps 1 \
-    --tf32 True \
-    --model_max_length 2048 \
-    --q_lora False \
-    --deepspeed $PATH_TO_DEEPSPEED_CONFIG \
-    --gradient_checkpointing True \
-    --flash_attn False
+    --model_max_length 512 \
+    --q_lora True \
+    --gradient_checkpointing False \
+    --flash_attn True
